af87e0d9dfe6c388690d0f298b34805e9692c584
little-qiang <346669517@qq.com>
2017-05-04 19:24:45 +0800

    add pcntl

--- main.php
+++ main.php
@@ -9,9 +9,14 @@ $num_process = isset($argv[2]) ? $argv[2] : 4;
 $allCartData = require('./data/cart.php');
 
 $start = microtime(true);
+
 $work = new Work($allCartData);
-$task = new Task($work, $num_process);
-$task->run($num_count);
+if(extension_loaded('pcntl')){
+	$task = new Task($work, $num_process);
+	$task->run($num_count);
+} else {
+	$work->runWorker($num_count);
+}
 $end = microtime(true);
 
 printf('cost %.2fs', $end-$start);
